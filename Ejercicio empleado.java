518f361c79d5ba15ce0412ce3a186f0ad01a9683
CristianRubiano <cristian.rubiano.samudio@gmail.com>
2022-01-20 17:51:04 +0100

    primer commit

---
+++ Ejercicio empleado.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Empleado {
+    final int password = 1234;
+    Scanner sc = new Scanner(System.in);
+    String nombre;
+    int edad;
+    double sueldo;
+
+    public Empleado(String nombre, int edad, double sueldo) {
+
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sueldo = sueldo;
+
+    }
+
+    public void Preguntar_Sueldo(int n1) {
+
+        if (n1 == password) {
+
+            System.out.println("Contraseña correcta");
+            System.out.println(sueldo);
+
+        } else {
+
+            System.out.println("Contraseña incorrecta");
+
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
+
